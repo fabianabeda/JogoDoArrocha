09eb40eaf6489dd28b3e3f6ba4dbe1dd4a02d04e
Fabiana Beda <82904498+fabianabeda@users.noreply.github.com>
2022-09-10 15:42:32 -0300

    tratamento de erro

--- app/src/main/java/br/eti/valeria/arrocha/MainActivity.kt
+++ app/src/main/java/br/eti/valeria/arrocha/MainActivity.kt
@@ -7,13 +7,16 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import android.widget.LinearLayout
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var etNumero: EditText
-    private lateinit var  bnArrocha: Button
+    private lateinit var bnArrocha: Button
     private lateinit var arrocha: Arrocha
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -25,25 +28,29 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    inner  class OnClickBotao: View.OnClickListener{
+    inner class OnClickBotao : View.OnClickListener {
         override fun onClick(p0: View?) {
-            var numero = this@MainActivity.etNumero.text.toString().toInt()
-            var msg = this@MainActivity.arrocha.jogar(numero)
+            try {
+                var numero = this@MainActivity.etNumero.text.toString().toInt()
+                var msg = this@MainActivity.arrocha.jogar(numero)
 
-            if (this@MainActivity.arrocha.getStatus() == Status.EXECUTANDO) {
-                Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
-            } else {
+                if (this@MainActivity.arrocha.getStatus() == Status.EXECUTANDO) {
+                    Toast.makeText(this@MainActivity, msg, Toast.LENGTH_SHORT).show()
+                } else {
                     //Navagar para outra tela
-                val intent = Intent(this@MainActivity, ResultadoActivity::class.java).apply {
-                    putExtra("JOGO", this@MainActivity.arrocha)
-                }
+                    val intent = Intent(this@MainActivity, ResultadoActivity::class.java).apply {
+                        putExtra("JOGO", this@MainActivity.arrocha)
+                    }
                     //intent.putExtra("JOGO",this@MainActivity.arrocha)
-                startActivity(intent)
-                this@MainActivity.arrocha = Arrocha()
-            }
+                    startActivity(intent)
+                    this@MainActivity.arrocha = Arrocha()
+                }
 
-            this@MainActivity.etNumero.setText("")
-
+                this@MainActivity.etNumero.setText("")
+            }catch (e: NumberFormatException){
+                Toast.makeText(this@MainActivity, "Digite um número válido", Toast.LENGTH_SHORT).show()
             }
+        }
     }
+
 }
